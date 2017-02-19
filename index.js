0c6e50e8e12fce3460ec314e8aaac517b791bb3e
AyedhSh <alshmary_ad@yahoo.com>
2017-02-19 18:55:58 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function theTruth(){return true; debugger;}
+
+function sayHey(){return "Hey!";}
+
+function sayHeyFriend(name){return `Hey, ${name}!`;}
+
+function favoriteIceCream(icecreamFlavor){return 'I love ' + icecreamFlavor ;}
+
+function shouting(string){	return string.toUpperCase();}
+
+function roundDown(number){return Math.floor(number);}
